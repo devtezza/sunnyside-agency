0dc6341846fe7913bcd0e7942a7b365a9dae429b
nandotezza <fernando.tezza@gmail.com>
2021-08-19 11:09:23 -0300

    Delete additional svg logo file. Change svg original logo file (set its color to black and apply css filter to customize it)

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
-const menuHamburger = document.querySelector('.menu-hamburger');
-const navbarMenu = document.querySelector('.menu');
-const outsideMenu = document.querySelector('.outside-menu');
+const menuHamburger = document.querySelector('.menu-hamburger')
+const navbarMenu = document.querySelector('.menu')
+const outsideMenu = document.querySelector('.outside-menu')
 
 
 // Open and Close navbarMenu by clik on menuHambuerger
-menuHamburger.addEventListener('click', toggleMenu);
+menuHamburger.addEventListener('click', toggleMenu)
 // Open and Close navbarMenu by Click Outside
-outsideMenu.addEventListener('click', toggleMenu);
+outsideMenu.addEventListener('click', toggleMenu)
 
 // Toggle Menu Function
 function toggleMenu() {
    
-    navbarMenu.classList.toggle('active');
-    menuHamburger.classList.toggle('active');
-    outsideMenu.classList.toggle('active');
-    document.body.classList.toggle('scrolling');
+    navbarMenu.classList.toggle('active')
+    menuHamburger.classList.toggle('active')
+    outsideMenu.classList.toggle('active')
+    document.body.classList.toggle('scrolling')
     
 }
 
@@ -22,13 +22,13 @@ function toggleMenu() {
 function resizeScreen() {
    // If navbarMenu is Open, Close It
    if (navbarMenu.classList.contains('active')) {
-      toggleMenu();
+      toggleMenu()
    }
   
 }
 
 window.addEventListener('resize', () => {
    if (this.innerWidth > 768) {
-      resizeScreen();
+      resizeScreen()
    }
-});
+})
